78ccd5ef5c270d4afc8f15bba88dc0201fd9b85e
elrm921 <pocketrocket03@gmail.com>
2022-06-17 20:38:16 +0300

    updated

--- main.cpp
+++ main.cpp
@@ -6,42 +6,42 @@ class Figure {
             this->name = "Фигура";
             this->sides = 0;
         }
-        std::string get() {
-            return this->name;
-        }
-        int count() {
-            return this->sides;
+        void print() {
+            std::cout << name << "\n";
+            std::cout << "Количество сторон: " << sides << "\n";
         }
     protected:
+        Figure(std::string name, int sides) {
+            this->name = name;
+            this->sides = sides;
+        }
+    private:
         std::string name;
         int sides;
 };
 
 class Triangle : public Figure {
     public:
-        Triangle() {
-            this->name = "Треугольник";
-            this->sides = 3;
-        }
+        Triangle() : Figure("Треугольник", 3) {}
 };
 
 class Quadrangle : public Figure {
     public:
-        Quadrangle() {
-            this->name = "Четырехугольник";
-            this->sides = 4;
-        }
+        Quadrangle() : Figure("Четырехугольник", 4) {}
 };
+
+void print(Figure &figure) {
+    figure.print();
+}
 
 int main() {
     Figure f1;
     Triangle t1;
     Quadrangle r1;
 
-    std::cout << "Количество сторон:" << "\n";
-    std::cout << f1.get() << ": " << f1.count() << "\n";
-    std::cout << t1.get() << ": " << t1.count() << "\n";
-    std::cout << r1.get() << ": " << r1.count() << "\n";
+    print(f1);
+    print(t1);
+    print(r1);
 
     return 0;
 }
